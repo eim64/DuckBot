7c3538dca81f684e723d1688e0e94d3b7755973a
eim64 <eim.wco@gmail.com>
2018-04-11 19:09:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,24 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+var images = {'na': 'NORTH AMERICA :flag_us::flag_mx::flag_ca:', 'eu': "EUROPE :flag_eu:", 'anz':'UPSIDE DOWN :flag_au::flag_nz:'};
+var regions = Object.keys(images);
+    
+
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    if(message.content.startsWith("!matchmake")){
+       message.delete();
+       var params = message.content.split(' ');
+       if(params.length < 3 || regions.indexOf(params[1].toLower()) < 0 || !params[2].startWith("steam://joinlobby/312530/")) {invalidSyntax(message); return;}
+       
+       var maxWait = "and did not specify for how long he was gonna have and open lobby";
+       if(params.length > 3) maxWait = "and is prepared to wait a staggering "+(params.splice(3,params.length-3).join(' '))+" before he closes the lobby";
+       message.reply("Wants to get his ass handed to him:\n"+params[2]+"\n"+maxWait+"\nRegion: "+(images[params[1].toLower()]));
+    }
 });
+
+function invalidSyntax(message){
+    message.reply("invalid syntax!\n!matchmake <"+regions.join()+"> <invite link> <optional: max wait message>");
+}
 
 client.login(process.env.BOT_TOKEN);
