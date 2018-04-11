33c64ac37e24d1b9523b03a33326a01ced597471
eim64 <eim.wco@gmail.com>
2018-04-11 19:39:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,22 +5,24 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-var images = {'na': 'NORTH AMERICA :flag_us::flag_mx::flag_ca:', 'eu': "EUROPE :flag_eu:", 'anz':'UPSIDE DOWN :flag_au::flag_nz:'};
+var images = {'na': 'https://cdn.discordapp.com/attachments/429801984460062720/433673377270726657/Uplink_-_World_Map_NA.gif', 
+              'eu': "https://cdn.discordapp.com/attachments/429801984460062720/433673470614831134/Uplink_-_World_Map_EU.gif", 
+              'anz':'https://cdn.discordapp.com/attachments/429801984460062720/433673374934368276/Uplink_-_World_Map_ANZ.gif'};
 var regions = Object.keys(images);
     
 
 client.on('message', message => {
-    if(message.content.startsWith("!matchmake")){
+    if(message.channel.name === "matchmaking" && message.content.startsWith("!matchmake")){
        if(message.deletable)
           message.delete();
-       console.log(message.channel.id+": name = "+message.channel.name);
+       
         
        var params = message.content.split(' ');
        if(params.length < 3 || regions.indexOf(params[1].toLowerCase()) < 0 || !params[2].startsWith("steam://joinlobby/312530/")) {invalidSyntax(message); return;}
        
-       var maxWait = "and did not specify for how long he was gonna have and open lobby";
+       var maxWait = "and did not specify for how long he was gonna have an open lobby";
        if(params.length > 3) maxWait = "and is prepared to wait a staggering "+(params.splice(3,params.length-3).join(' '))+" before he closes the lobby";
-       message.reply("Wants to get his ass handed to him:\n"+params[2]+"\n"+maxWait+"\nRegion: "+(images[params[1].toLowerCase()]));
+       message.reply("Wants to get his ass handed to him:\n"+params[2]+"\n"+maxWait,{files:[images[params[1].toLowerCase()]]});
     }
 });
 
