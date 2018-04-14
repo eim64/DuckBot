ad35bb7462a3ecb79296561d5deb5bc06b021d2c
eim64 <eim.wco@gmail.com>
2018-04-14 08:20:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,73 +11,83 @@ var regions = Object.keys(images);
 
 
 var openLobbies = new Map();
-var previousLobbies = [];
+
+var commands = new Map();
+
+var MMRole;
 
 client.on('message', message => {
     if(message.channel.name === "matchmaking"){
-        var loweredContent = message.content.toLowerCase();
         
-        var MMRole = message.guild.roles.find("name", "WaitingForAMatch");
-        if( loweredContent.startsWith("!matchmake")){
-            if(message.deletable) message.delete();
-       
-            if(openLobbies.has(message.author.id)) {
-                message.reply("You supposedly already have a lobby open! Type ***!closelobby*** to remove previous message");
-            }
-            
-            var params = message.content.split(' ');
-            var userRegion, inviteLink, waitTime,linkParams;
-            
-            if(params.length < 4 ||                                                 //not enough parameters
-            regions.indexOf((userRegion = params[1].toLowerCase())) < 0 ||          //is not a valid region
-            !(inviteLink = params[2]).startsWith("steam://joinlobby/312530/")||     //invite link is not valid
-            (waitTime = parseInt(params[3])) == NaN||                               //wait time is not a number
-            (linkParams = inviteLink.split('/')).length != 6) {                     //invite link lacks lobbyID/userID
-                
-                invalidSyntax(message); 
-                return;
-            }
-            
-            if(previousLobbies.indexOf(linkParams[4]) < 0){
-                previousLobbies.push(linkParams[4]);        
-            }else return;                                   //already used lobbyID
-            
-            var waitMilliseconds = waitTime*60000;
-            
-            message.channel.send("Hey "+MMRole+", "+message.author+" Wants to get his ass handed to him:\n"+params[2]+"\nHe will supposedly close his lobby in "+waitTime+" minutes.",{files:[images[params[1].toLowerCase()]]})
-                .then(sent=>{
-                    openLobbies.set(message.author.id,sent);
-                    sent.delete(waitMilliseconds).then(setTimeout(function(){
-                        openLobbies.delete(message.author.id);
-                    },waitMilliseconds));
-                });
+        var params = message.content.toLowerCase().split(' ');
+        if(!commands.has(params[0])) return;
         
-        }else if(loweredContent.startsWith("!joinmm")){
-            if(message.deletable) message.delete();
-            
-            if(message.member.roles.has(MMRole.id)) return;
-            
-            message.reply('Is now waiting for matches');
-            message.member.addRole(MMRole);
-        
-        }else if(loweredContent.startsWith("!leavemm")){
-            if(message.deletable) message.delete();
-            
-            if(!message.member.roles.has(MMRole.id)) return;
-            
-            message.reply('is no longer waiting for a match');
-            message.member.removeRole(MMRole);
-            
-        }else if(loweredContent.startsWith("!closelobby")){
-            if(message.deletable) message.delete();
-            if(!openLobbies.has(message.author.id)) {console.log("user not found!"); return;}
-            
-            var msg = openLobbies.get(message.author.id);
-            msg.delete();
-            openLobbies.delete(message.author.id);
-        }
+        MMRole = message.guild.roles.find("name", "WaitingForAMatch");
+        if(message.deletable) message.delete();
+        commands.get(params[0])(message,params.splice(1,params.length-1));
     }
 });
+
+commands.set("!matchmake",function(message,params){
+   if(openLobbies.has(message.author.id)) {
+       message.reply("You supposedly already have a lobby open! Type ***!closelobby*** to remove previous message");
+       return;
+   }
+            
+   var userRegion, inviteLink, waitTime,linkParams;
+            
+   if(params.length < 3 ||                                                 //not enough parameters
+   regions.indexOf((userRegion = params[0].toLowerCase())) < 0 ||          //is not a valid region
+   !(inviteLink = params[1]).startsWith("steam://joinlobby/312530/")||     //invite link is not valid
+   (waitTime = parseInt(params[2])) == NaN||                               //wait time is not a number
+   (linkParams = inviteLink.split('/')).length != 6) {                     //invite link lacks lobbyID/userID
+                
+        invalidSyntax(message); 
+        return;
+   }
+            
+   var waitMilliseconds = waitTime*60000;
+            
+   message.channel.send("Hey "+MMRole+", "+message.author+" Wants to get his ass handed to him:\n"+params[1]+"\nHe will supposedly close his lobby in "+waitTime+" minutes.",{files:[images[params[0].toLowerCase()]]})
+    .then(sent=>{
+         openLobbies.set(message.author.id,sent);
+         sent.delete(waitMilliseconds).then(setTimeout(function(){
+              openLobbies.delete(message.author.id);
+         },waitMilliseconds));
+    }); 
+});
+
+commands.set("!closelobby",function(message,params){
+   if(!openLobbies.has(message.author.id)) {console.log("user not found!"); return;}
+            
+   var msg = openLobbies.get(message.author.id);
+   msg.delete();
+   openLobbies.delete(message.author.id); 
+});
+
+
+
+function joinmm(message,params){
+ if(message.member.roles.has(MMRole.id)) return;
+            
+ message.reply('Is now waiting for matches');
+ message.member.addRole(MMRole);   
+}
+
+function leavemm(message,params){
+    if(!message.member.roles.has(MMRole.id)) return;
+            
+    message.reply('is no longer waiting for a match');
+    message.member.removeRole(MMRole);
+}
+
+commands.set("!joinmm",joinmm);
+commands.set("!joinmatchmaking",joinmm);
+commands.set("!leavemm",leavemm);
+commands.set("!leavematchmaking",leavemm);
+
+
+
 
 function invalidSyntax(message){
     message.reply("invalid syntax!\n!matchmake <"+regions.join()+"> <invite link> <time until close (minutes)>");
